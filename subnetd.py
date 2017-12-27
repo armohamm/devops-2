51c2bde7d7e8b24fcc8a909bbf5a563c84254cd2
dexterpark <ddexterpark@icloud.com>
2017-12-26 23:00:26 -0800

    Updating subnetd

--- subnetd.py
+++ subnetd.py
@@ -1,8 +1,12 @@
 from netaddr import IPAddress, IPNetwork
+from subprocess import run
 import sys
+
+run("pip install netaddr", shell=True, check=True)
 
 # https://media.readthedocs.org/pdf/netaddr/latest/netaddr.pdf
 # netaddr has so much potential for future scripts such as re-iping, and IPAM. Must investigate further.
+
 
 def main(subnet):
     ipn =  IPNetwork(subnet)
@@ -29,23 +33,11 @@ def main(subnet):
         print('HostMin:   ', ipn.network + 1)
         print('HostMax:   ', ipn.broadcast-1)
         print('Address Space:', ipn.size)
-'''
-Address:   192.168.0.1           11000000.10101000.00000000 .00000001
-Netmask:   255.255.255.0 = 24    11111111.11111111.11111111 .00000000
-Wildcard:  0.0.0.255             00000000.00000000.00000000 .11111111
-=>
-Network:   192.168.0.0/24        11000000.10101000.00000000 .00000000 (Class C)
-Broadcast: 192.168.0.255         11000000.10101000.00000000 .11111111
-HostMin:   192.168.0.1           11000000.10101000.00000000 .00000001
-HostMax:   192.168.0.254         11000000.10101000.00000000 .11111110
-Hosts/Net: 254                   (Private Internet)
-'''
 
 if __name__ == "__main__":
     subnet = sys.argv[1] if len(sys.argv) > 1 else sys.exit(0)
     main(subnet)
     print('''
-    
    //         ,&&&&&&         
  (((         %&&&&&&&%     ((
  ((     &&&&&&&&&&&&&&&    ((
